3b01c7e04f9c2c6ae7ba32553e93957313568fb2
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2026-05-20 21:46:05 -0400

    Create  build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    kotlin("jvm") version "2.0.21"
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    alias(libs.plugins.android.library) apply false
+    kotlin("plugin.serialization") version "2.1.0"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
